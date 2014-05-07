d1cb50dca27e792b724b3638b0f1c0da2075461b
Adam Harter <atbh79@mail.mst.edu>
2014-05-07 15:29:02 -0500

    Added pseduocode

--- proj2/main.c
+++ proj2/main.c
@@ -1,7 +1,39 @@
 #include "REG932.H"
 #include "uart.h"
 
+//mode 0 = play song, mode 1 = keyboard
+unsigned char mode = 0;
+
+void interrupt0(void) interrupt 1
+{
+	switch(mode)
+	{
+	 	case 0:	 //playing song
+		//go to next tone if overflow number reached
+		break;
+
+		case 1:  //keyboard note
+		//complement bit
+		break;
+
+		default: //unknown error
+		//this should never be reached
+		break;
+	}
+}
+
 void main(void)
 {
-   ;
+    while(1)
+    {
+		//check for buttons pressed and set mode acordingly
+		//call function to turn off interrupts
+		//turn on needed interrupts
+		//if keyboard mode check for additional buttons
+    }
+}
+
+void setTimer(unsigned char count)
+{
+   //sets timer to play frequency
 }
